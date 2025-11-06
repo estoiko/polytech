d9d089aa3091db1430b1b2f29009faecfd233e96
Elisey Stoiko <eliseystoiko@gmail.com>
2025-11-06 12:58:07 +0300

    Create calc.sh

--- GNU_Linux/calc.sh
+++ GNU_Linux/calc.sh
@@ -4,9 +4,23 @@ shift 1
 while [ $#  -gt 1 ]; do
     case $1 in
         +) res=`echo "scale=6;$res + $2" | bc`;;
+	-) res=`echo "scale=6;$res - $2" | bc`;; 
+	x) res=`echo "scale=6;$res * $2" | bc`;;
+        /) 
+	if [ `echo "$2 == 0" | bc` = "1" ]; then 
+		res="d"
+		break
+	fi
+	res=`echo "scale=6;$res / $2" | bc`;;
+	*) res="!"; break;;
     esac
     shift 2
 done
-echo "result - $res"
 
-# bc .0 == 0 при проверке деления на ноль 
+if [ $res = "!" ]; then
+	echo "Неизвестная операция"
+elif [ $res = "d" ]; then
+ 	echo "Ошибка: деление на 0"
+else 	
+	echo "result = $res"
+fi 
